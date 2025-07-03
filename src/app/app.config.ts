0a99d4922bdd8d099aff8b83f0f465dea7617ac1
Daniel Cortes <danncortes@gmail.com>
2025-07-03 09:31:43 +0200

    ci: update deploy workflow and remove client hydration
    Update publish directory path in GitHub workflow to target browser subfolder
    Remove deprecated provideClientHydration from app config as it's no longer needed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,6 @@ import {
     withFetch,
     withInterceptors,
 } from '@angular/common/http';
-import { provideClientHydration } from '@angular/platform-browser';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './interceptors/auth.interceptor';
@@ -14,7 +13,7 @@ export const appConfig: ApplicationConfig = {
     providers: [
         provideZoneChangeDetection({ eventCoalescing: true }),
         provideRouter(routes),
-        provideClientHydration(), // Remove withEventReplay()
+
         provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
     ],
 };
